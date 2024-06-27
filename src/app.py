7c2f62ab4989419c9692398c018c7916ead4d925
Joe DeCapo <joe@polka.cat>
2024-06-26 21:22:23 -0500

    Fix collapse buttons

--- src/app.py
+++ src/app.py
@@ -9,7 +9,15 @@ def home():
     with open("page.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     parsed_data = parse_html.parse_html(html_content)
-    rendered_template = render_template('index.html', texts=parsed_data)
+
+    indexed_data = [{'index': idx + 1, 'content': text} for idx, text in enumerate(parsed_data)]
+
+    rendered_template = render_template('index.html', texts=indexed_data)
+
+    with open("index.html", "w", encoding="utf-8") as file:
+        file.write(rendered_template)
+    print("HTML content rendered and saved to index.html")
+
     return rendered_template
 
 
