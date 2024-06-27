c4138f69c4092933f880ffe14b1722edd65437f7
Joe DeCapo <joe@polka.cat>
2024-06-26 19:12:26 -0500

    Fix fetch

--- src/fetch_html.py
+++ src/fetch_html.py
@@ -1,14 +1,17 @@
 import requests
 
 def fetch_html(url):
-    response = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.text
     else:
         raise Exception(f"Failed to fetch page. Status code: {response.status_code}")
 
 if __name__ == "__main__":
-    url = "http://www.bucketheadpikes.com"
+    url = "https://www.bucketheadpikes.com"
     html_content = fetch_html(url)
     with open("page.html", "w", encoding="utf-8") as file:
         file.write(html_content)
