58aa1460a79040ecc54296bb9074aaa0a47637b7
Joe DeCapo <joe@polka.cat>
2024-07-13 16:05:53 -0500

    Add freeze script

--- src/constants.py
+++ src/constants.py
@@ -5,5 +5,5 @@ REPO_DIR = current_file_path = Path(__file__).parent.parent
 STATIC_CRAWL_DIR = REPO_DIR / 'static_site'
 BUILD_DIR = REPO_DIR / 'build'
 
-os.makedirs(os.path.dirname(STATIC_CRAWL_DIR), exist_ok=True)
-os.makedirs(os.path.dirname(BUILD_DIR), exist_ok=True)
+os.makedirs(STATIC_CRAWL_DIR, exist_ok=True)
+os.makedirs(BUILD_DIR, exist_ok=True)
