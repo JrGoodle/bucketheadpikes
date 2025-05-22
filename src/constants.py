995251b11da40057e0f4976def497dd0d9d37508
Joe DeCapo <joe@polka.cat>
2025-05-21 20:11:08 -0500

    Start fresh with cursor

--- src/constants.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import os
-
-REPO_DIR = current_file_path = Path(__file__).parent.parent
-STATIC_CRAWL_DIR = REPO_DIR / 'static_site'
-BUILD_DIR = REPO_DIR / 'build'
-
-os.makedirs(STATIC_CRAWL_DIR, exist_ok=True)
-os.makedirs(BUILD_DIR, exist_ok=True)
--- src/model/index.py
+++
@@ -1,7 +0,0 @@
-class Site:
-    def __init__(self):
-        self.tour = ''
-        self.autographs = ''
-        self.pamela = ''
-        self.henry = ''
-        self.pikes = ''
